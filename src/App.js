1bfbc354f02e2a53740f601db03bf1daba00e977
avishkakoswatta <avishkakkoswaththa@gmail.com>
2024-06-28 23:04:44 +0530

    third

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome </h1>
+        <h1>Welcome</h1>
       </header>
+      <div className="Login">
+        <h2>Login</h2>
+        <form>
+          <div>
+            <label htmlFor="username">Username-</label>
+            <input type="text" id="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
